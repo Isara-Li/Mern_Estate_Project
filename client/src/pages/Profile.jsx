981bb8b6f61de53c48c786704de0142b619c6315
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-07 21:12:21 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+
 
 export default function Profile() {
+  const {currentUser} = useSelector(state => state.user)
   return (
-    <div>Profile</div>
+    <div className='max-w-lg mx-auto'>
+    <h1 className='text-3xl font-mono font-semibold text-center py-7'>Profile</h1>
+    <form className='flex flex-col gap-4'>
+      <img src= {currentUser.avatar} alt="pic" className='rounded-full h-24 w-24 pbject-cover cursor-pointer self-center' /> {/* self center is used for images */}
+      <input type="text" placeholder='User name' className='border p-3 rounded-lg' id='username' />
+      <input type="Etext" placeholder='Email' className='border p-3 rounded-lg' id='email' />
+      <input type="text" placeholder='Password' className='border p-3 rounded-lg' id='password' />
+      <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-60'>Update</button>
+
+    </form>
+    <div className='flex justify-between mt-5'>
+      <p className='text-red-700'>Delete account</p>
+      <p className='text-red-700'>Sign out</p>
+    </div>
+  </div>
   )
 }
