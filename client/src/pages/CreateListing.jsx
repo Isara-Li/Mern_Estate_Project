1f129b2ba41c787a7a1421ee281f50445aa0dc11
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-11 19:02:53 +0530

    Update CreateListing.jsx

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -5,14 +5,18 @@ import { getDownloadURL,
   ref,
   uploadBytesResumable,} from 'firebase/storage';
 import {app} from '../firebase';
+import { set } from 'mongoose';
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
   const [formData, setFormdata] = useState({
      imageURLs: [],
   })
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(false); // to show the error message if the upload fails
   const handleImageUpload = () => {
-      if (files.length > 0 && files.length < 7 ) {
+      if (files.length > 0 && files.length + formData.imageURLs.length < 7 ) {
+            setUploading(true);
            const imagePromises = [];
 
             for (let i = 0; i < files.length; i++) {
@@ -21,7 +25,19 @@ export default function CreateListing() {
             Promise.all(imagePromises).then((downloadURLs) => {
               console.log(downloadURLs);
               setFormdata({ ...formData, imageURLs: formData.imageURLs.concat(downloadURLs) });
-            }) // concat part is to add the new images to the existing images
+              setUploadError(false);
+              setUploading(false);
+            }).catch((error) => {
+              console.log(error);
+              setUploadError("Image size should be less than 2MB");
+              setUploading(false);
+            })
+            
+      }
+      else{
+        setUploadError("Please select only 6 images to upload.");
+        console.log("Isara");
+        setUploading(false);
       }
   }
   const storeImage = async (file) => {
@@ -48,6 +64,12 @@ export default function CreateListing() {
       );
     });
   };
+  const handleRemoveImage = (index) => {
+    setFormdata({
+      ...formData,
+      imageURLs: formData.imageURLs.filter((url, i) => i !== index),
+    });
+  }
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -155,14 +177,37 @@ export default function CreateListing() {
         </div>
         <div className="flex flex-col flex-1 gap-4">
           <p className='font-semibold'>Images:
-          <span className='font-normal text-gray-600 ml-2'>The first image will be the cover (max 6)</span>
+          <span className='font-normal text-gray-600 ml-2'>The first image will be the cover photo. (max 6)</span>
           </p>
           <div className="flex gap-4">
             <input onChange={(e)=>setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
-            <button type='button'  onClick={handleImageUpload} className='p-3 text-white bg-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+            <button type='button'  onClick={handleImageUpload} className='p-3 text-white bg-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>{uploading ? 'Uploading' : 'Upload'}</button>
           </div> {/* type={button} prevents the submission of the whole form */}
+          <p className='text-red-700'>{uploadError ? uploadError : ""}</p>
+          {
+            formData.imageURLs.length > 0 && formData.imageURLs.map((url, index) => (
+              <div
+              key={url}
+              className='flex justify-between p-3 border bg-slate-100 items-center rounded-lg'
+            >
+              <img
+                src={url}
+                alt='listing image'
+                className='w-20 h-20 object-contain rounded-lg' 
+              /> {/* object contain preserves the original aspect ratio */}
+              <button
+                type='button'
+                onClick={() => handleRemoveImage(index)} 
+                className='p-3 bg-red-700 text-white rounded-lg uppercase hover:opacity-75'
+              > {/* this is the default way to call the funtions when there is a parameter to pass onClick={function(parameter)} call the function even we don't click on it */}
+                Delete
+              </button>
+            </div>
+            ))
+          }
         <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
         </div>
+        
       </form>
     </main>
   );
