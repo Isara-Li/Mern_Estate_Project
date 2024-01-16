d17a7b5e2b91b6837f3d295cda0734aaad498696
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-16 23:35:36 +0530

    creating get listing server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -44,6 +45,20 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json({
       message: "User has been deleted...",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(
+      new Error("You are not allowed to get the listing of this user!")
+    );
+  }
+  try {
+    const listing = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getListing,
 } from "../controllers/user.controller.js";
 import { verrifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verrifyToken, updateUser);
 router.delete("/delete/:id", verrifyToken, deleteUser); // Usually this id parameter is not needed. But in this case we need it to check if the user is allowed to delete the user.
+router.get("/listing/:id", verrifyToken, getListing);
 
 export default router;
