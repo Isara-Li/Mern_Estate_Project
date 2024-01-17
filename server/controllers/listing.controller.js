cbb8230879e47e46306e58713a552d8b674e1c7e
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-17 19:15:38 +0530

    Delete Listing server route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -11,3 +11,23 @@ export const createListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (req.user.id !== listing.userRef) {
+    return next(
+      new Error("You are not allowed to delete the listing of this user!")
+    );
+  }
+  if (!listing) {
+    return next(new Error("No listing found!"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Listing has been deleted...",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controllers/listing.controller.js";
 import { verrifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verrifyToken, createListing);
-
+router.delete("/delete/:id", verrifyToken, deleteListing);
 export default router;
