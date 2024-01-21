9b50079f6d36ac81ac439800d272006896052e92
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-21 23:30:24 +0530

    Search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/listing/:listingID" element={<Listing />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing/>}/> 
