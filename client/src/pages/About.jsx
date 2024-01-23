ec6071b85f8a906d49dfc0bde33e5e04bbd89cc1
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-01-23 18:35:11 +0530

    Completing the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-export default function About() {
+export default function AnotherPage() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Discover Tranquil Haven Realty</h1>
+      <p className='mb-4 text-slate-700'>Tranquil Haven Realty stands as a premier real estate agency, specializing in facilitating seamless transactions for clients looking to buy, sell, or rent properties in highly sought-after neighborhoods. Our dedicated team of seasoned agents is committed to delivering outstanding service, ensuring a stress-free experience throughout the real estate journey.</p>
+      <p className='mb-4 text-slate-700'>
+        At Tranquil Haven Realty, our mission is to empower clients to realize their real estate aspirations. We achieve this through expert advice, personalized service, and an in-depth knowledge of the local market. Whether your goal is buying your dream home, selling a property, or finding the perfect rental, we're here to guide you through every step.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team boasts a wealth of experience and expertise in the real estate industry. We are unwaveringly dedicated to delivering the highest standard of service to our clients. We firmly believe that each real estate transaction should be a gratifying and exhilarating experience, and we're committed to transforming that belief into reality for each client we serve.</p>
+    </div>
+  );
 }
